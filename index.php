21123f4d15aa5000d261b6f460bb4a4fac3f44d7
JoseRinaldofilho <jose.vasconcelos@basis.com.br>
2021-09-28 15:00:10 -0300

    index em andamento

--- index.php
+++ index.php
@@ -11,7 +11,29 @@
     <title>Projeto Dev WEB</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+<nav class="navbar navbar-default navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#" >RinaldoDev°</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav nav-right me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active"   href="#">sobre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" >Contato</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
 
 <!-- Optional JavaScript; choose one of the two! -->
 
